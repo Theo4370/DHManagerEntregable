773d7071b97699e79c626972674b3bcbea1628d8
Theo <theomusumeci@gmail.com>
2020-03-20 15:21:05 -0300

    ParteB, clase Curso, items 1, 2, 3, 4.

---
+++ src/com/company/Curso.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Curso {
+    private String nombreDeCurso;
+    private Integer codigoDeCurso;
+
+    public Curso(String nombreDelCurso, Integer codigoDeCurso) {
+        this.nombreDeCurso = nombreDelCurso;
+        this.codigoDeCurso = codigoDeCurso;
+    }
+
+    public String getNombreDelCurso() {
+        return nombreDeCurso;
+    }
+
+    public void setNombreDelCurso(String nombreDelCurso) {
+        this.nombreDeCurso = nombreDelCurso;
+    }
+
+    public Integer getCodigoDeCurso() {
+        return codigoDeCurso;
+    }
+
+    public void setCodigoDeCurso(Integer codigoDeCurso) {
+        codigoDeCurso = codigoDeCurso;
+    }
+
+    public boolean equals(Object cursoAComparar) {
+        if (!(cursoAComparar instanceof Curso)) {
+            return false;
+        }
+
+        Curso otroCurso = (Curso) cursoAComparar;
+        return otroCurso.getCodigoDeCurso().equals(this.codigoDeCurso);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,12 @@ public class Main {
         // write your code here
 
         Alumno unAlumno = new Alumno("Theo", "Musumeci", 55115);
-        Alumno otroAlumno = new Alumno("Juan", "Perez", 55118);
+        Alumno otroAlumno = new Alumno("Juan", "Perez", 55115);
         otroAlumno.equals(unAlumno);
         System.out.println("" + otroAlumno.equals(unAlumno));
-    }
+
+        Curso unCurso = new Curso("Mamtematica",1589);
+        Curso otroCurso = new Curso("Historia",1589);
+        System.out.println(""+otroCurso.equals(unCurso));
+        System.out.println(""+unCurso.equals(otroCurso));    }
 }
