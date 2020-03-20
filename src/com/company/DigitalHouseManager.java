ceb2f0e2df7e05fe076d97cf101ea7a9fb77fdb5
Theo <theomusumeci@gmail.com>
2020-03-20 17:07:23 -0300

    Parte G, clase DigitalHouseManager

---
+++ src/com/company/DigitalHouseManager.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.Profesor.Profesor;
+
+import java.util.List;
+
+/**
+Ni constructor, ni getters ni setters fueron creados ya que la parte G no lo pide.
+*/
+
+public class DigitalHouseManager {
+    private List<Alumno> listaDeAlumnos;
+    private List<Profesor> listaDeProfesores;
+    private List<Inscripcion> listaDeInscripciones;
+;}
