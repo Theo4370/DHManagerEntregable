26206876bf30511ce36ad214a2e4bafb0fa6a290
Theo <theomusumeci@gmail.com>
2020-03-20 17:05:29 -0300

    Parte G, clase DigitalHouseManager

---
+++ src/com/company/DigitalHouseManager.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.Profesor.Profesor;
+
+import java.util.List;
+
+public class DigitalHouseManager {
+    private List<Alumno> listaDeAlumnos;
+    private List<Profesor> listaDeProfesores;
+    private List<Inscripcion> listaDeInscripciones;
+;}
