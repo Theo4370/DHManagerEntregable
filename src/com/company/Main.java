bbe1018bc60e399cc76933c480994e06fd77246c
Theo <theomusumeci@gmail.com>
2020-03-21 18:04:37 -0300

    Parte J, creo el main con todo lo pedido.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,26 +10,50 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-
 
         DigitalHouseManager nuevoManager = new DigitalHouseManager();
-        nuevoManager.altaCurso("Matematica", 8756, 7);
-        //nuevoManager.bajaCurso(8756);
-        nuevoManager.altaCurso("Fisica", 8796, 1);
-        nuevoManager.altaCurso("Quimica", 8706, 7);
 
+        //Creo los profesores
 
         nuevoManager.altaProfesorAdjunto("Tomas", "Contreras", 3819, 2);
-        nuevoManager.altaProfesorTitular("Pedro", "Fatti", 4370, "Fisica cuantica");
-        //System.out.println("" + nuevoManager.getListaDeProfesores());
-        //nuevoManager.bajaProfesor(3819);
-        //System.out.println("" + nuevoManager.getListaDeProfesores());
-        nuevoManager.altaAlumno("Theo", "Musumeci", 55117);
-        nuevoManager.altaAlumno("Juan", "Perez", 55115);
-        nuevoManager.inscribirAlumno(55115, 8796);
-        nuevoManager.inscribirAlumno(55117, 8796);
-nuevoManager.asignarProfesores(8796,4370,3819);
+        nuevoManager.altaProfesorAdjunto("Jose", "Verdini", 4178, 5);
+        nuevoManager.altaProfesorTitular("Pedro", "Fatti", 4370, "Redes sociales");
+        nuevoManager.altaProfesorTitular("Chester", "Gonzalez", 1856, "Diseño de paginas web");
+
+        //Creo los cursos
+
+        nuevoManager.altaCurso("Full Stack", 20001, 3);
+        nuevoManager.altaCurso("Android", 20002, 2);
+
+        //Asigo profesores a los cursos
+
+        nuevoManager.asignarProfesores(20001, 1856, 4178);
+        nuevoManager.asignarProfesores(20002, 4370, 3819);
+
+        //Doy de alta 3 alumnos
+
+        nuevoManager.altaAlumno("Theo", "Musumeci", 113);
+        nuevoManager.altaAlumno("Juan", "Perez", 365);
+        nuevoManager.altaAlumno("Tomas", "Nuñez", 867);
+
+        //Inscribo 2 alumnos en el curso Full Stack
+
+        nuevoManager.inscribirAlumno(113, 20001);
+        nuevoManager.inscribirAlumno(867, 20001);
+
+        //Inscribo 3 alumnos al curso Android
+
+        nuevoManager.inscribirAlumno(113, 20002);
+        nuevoManager.inscribirAlumno(867, 20002);
+        nuevoManager.inscribirAlumno(365, 20002);
+
+        //Doy de baja un profesor
+
+        nuevoManager.bajaProfesor(4178);
+
+        //Doy de baja un curso
+
+        nuevoManager.bajaCurso(20002);
 
     }
 }
